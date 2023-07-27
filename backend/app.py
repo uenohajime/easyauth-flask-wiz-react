5f5f86703b1256cb605189c5b1fd703dfc8081dc
Hajime Ueno <4659066+uenohajime@users.noreply.github.com>
2023-07-28 07:40:00 +0900

    add cors config

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
 
 @app.route("/", defaults={"path": "index.html"})
 @app.route("/<path:path>")
 def index_file(path):
     return app.send_static_file(path)
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 if __name__ == "__main__":
     print(app.url_map)
